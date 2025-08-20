7088c7f4a03561f52d17387dafc3d05ee2d19acf
V <valdemaras@aidocs.pro>
2025-08-20 09:18:30 +0300

    add greeting service

---
+++ GreetingService.java
@@ -0,0 +1,9 @@
+public class GreetingService {
+
+    public String getGreeting(String name) {
+        if (name == null || name.isEmpty()) {
+            return "Hello, Stranger!";
+        }
+        return "Hello, " + name + "!";
+    }
+}
